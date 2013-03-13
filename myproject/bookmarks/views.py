29ecda48bdceec996e54dfab5b41765faba80702
Karen Rustad <karen.rustad@gmail.com>
2013-03-12 17:41:07 -0700

    Added some simple views

--- myproject/bookmarks/views.py
+++ myproject/bookmarks/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+ 
+def index(request):
+    return HttpResponse("Hello, world. You're at the bookmarks index.")
+
+
+def tag(request, tag_name):
+    return HttpResponse("This is a tag: <strong>%s</strong>" % (tag_name,))
+    
