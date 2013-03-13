aad739be3ef75d82084285db7a885f571f6b1691
Karen Rustad <karen.rustad@gmail.com>
2013-03-12 17:44:18 -0700

    Made our first template-powered view

--- myproject/bookmarks/views.py
+++ myproject/bookmarks/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
  
 def index(request):
-    return HttpResponse("Hello, world. You're at the bookmarks index.")
+    context = {}
+    return render(request, 'index.html', context)
 
 
 def tag(request, tag_name):
