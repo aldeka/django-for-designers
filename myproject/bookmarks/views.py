d419118b8ba48fccc57297156b0890884121a72a
Karen Rustad <karen.rustad@gmail.com>
2013-03-12 20:45:56 -0700

    Made views get model data

--- myproject/bookmarks/views.py
+++ myproject/bookmarks/views.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from bookmarks.models import Bookmark, Tag
 
  
 def index(request):
-    context = {}
+    bookmarks = Bookmark.objects.all().order_by('-timestamp')[:10]
+    context = {
+        'bookmarks': bookmarks,
+    }
     return render(request, 'index.html', context)
 
 
 def tag(request, tag_name):
+    tag = Tag.objects.get(slug=tag_name)
+    bookmarks = tag.bookmarks.all()
     context = {
-        'tag': tag_name,
+        'tag': tag,
+        'bookmarks': bookmarks,
     }
     return render(request, 'tag.html', context)
