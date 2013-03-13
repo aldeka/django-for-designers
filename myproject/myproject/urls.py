d354524c7fcc0ff270deb179ee483846b5d6812d
Karen Rustad <karen.rustad@gmail.com>
2013-03-12 20:30:09 -0700

    Added login and logout functionality

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'^$', 'bookmarks.views.index', name='home'),
     url(r'^bookmarks/$', 'bookmarks.views.index', name='bookmarks_view'),
     url(r'^tags/([\w-]+)/$', 'bookmarks.views.tag'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 )
